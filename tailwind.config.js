ee29231cc3e3075d950487a206881b10f9e22e79
MhinHub <iskandarm20d@student.unhas.ac.id>
2023-12-01 18:37:49 +0800

    update: nextjs version

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,10 +17,4 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
 };
